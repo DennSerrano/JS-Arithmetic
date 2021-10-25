671fb2b5b12dda963d3ac846581ee862c5ddf82e
Dennserrano <dennserrano@gmail.com>
2021-10-25 14:29:22 +0200

    lev12/13

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+//Punkt vor Strich rechnung
+
+// let score = 5 + 5 * 10;
+// console.log('Ergebnis: ' + score);
+
+// Klammer wird zuerst gerechnet
+
+// let score = (5 + 5) * 10;
+// console.log('Ergebnis: ' + score);
+
+//Varaiable wird zum rechnen benutzt
+// let score = 5;
+// score = score + 10;
+// console.log('Ergebnis: ' + score);
+
+//Shortcut zu dem darüber
+// let score = 5;
+// score += 10;
+// console.log('Ergebnis: ' + score);
+
+// let zahl = 1;
+// zahl = zahl + 1;
+// zahl += 1;
+
+// Verwende Inkrement-Operatoren
+// zahl++;
+// console.log('increment: ' + zahl);
+
+// Verwende Dekrement-Operatoren
+// zahl--;
+// console.log('dekrement: ' + zahl);
+
+// let zahl = 5
+// zahl = zahl - 1;
+
+// console.log(zahl--);
+
+// let zahl = 5
+// zahl = zahl + 1;
+
+// console.log(zahl++);
+
+// 1_13
+
+// Addition
+let addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+
+// Subtraktion
+let subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+
+//Division
+let division_operator = 4 / 2;
+console.log("division: " + division_operator);
+
+// Modul: zeigt an, was der Rest sein würde.
+let modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
